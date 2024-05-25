f95c8b33c9842482a3f9a4c03465f92f80697c30
Badal Patidar <badalpatidar210@gmail.com>
2024-05-25 19:07:25 +0530

    added image using image.network widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-    backgroundColor: Colors.black,
-    appBar: AppBar(
-      backgroundColor: Color(0xff000000),
-      centerTitle: true,
-      title: Text("My Avatar App"),
-    ),
-    body: Center(
-      child: Image.asset("images/dashh.jpeg"),
-    ),
-  )));
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Color(0xff000000),
+          centerTitle: true,
+          title: Text("My App"),
+        ),
+        body: Center(
+          child: Image.network(
+              "https://picsum.photos/250?image=9"),
+        ),
+      )));
 }
