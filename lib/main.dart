1c898e7716525e7f6a660c9ae0dd6b43a8fe9209
Badal Patidar <badalpatidar210@gmail.com>
2024-05-29 15:00:04 +0530

    added another widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-      home: Scaffold(
-    backgroundColor: Colors.black,
-    appBar: AppBar(
-      backgroundColor: Color(0xff000000),
-      centerTitle: true,
-      title: Text("My Avatar App"),
-    ),
-    body: Center(
-      child: Image.asset("images/dashh.jpeg"),
-    ),
-  )));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    List<Text> a = [Text("hello"), Text("Badal")];
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            // backgroundColor: Colors.black,
+            appBar: AppBar(
+              // backgroundColor: Color(0xff000000),
+              centerTitle: true,
+              title: a[0],
+            ),
+            body: Stack(
+              // alignment: Alignment.topCenter,
+              // scrollDirection: Axis.vertical,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                  // child: Image.asset("images/dash.jpeg"),
+                ),
+                Positioned(
+                  left: 10.0,
+                  top: 20.0,
+                  child: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.blue,
+                    // child: Image.asset("images/dashh.jpeg"),
+                  ),
+                ),
+              ],
+            )));
+  }
+}
+
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.grey,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.red,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.blue,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.grey,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.red,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+// Container(
+// height: 100.0,
+// width: 100.0,
+// color: Colors.blue,
+// child: Image.asset("images/dashh.jpeg"),
+// ),
+
